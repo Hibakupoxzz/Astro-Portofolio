334851de198c9599fd7b60d94fe87e24a20f3be9
Hibakupoxzz <duospirits@gmail.com>
2025-11-03 00:21:25 +0700

    Nia gua lucukkk banget weh

--- src/components/Wave.jsx
+++ src/components/Wave.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import Wave from "react-wavify";
+import React from 'react'
+import Wave from 'react-wavify'
 
-export default function WaveBackground() {
+export default function WaveReact() {
   return (
-    <div style={{ position: "relative", width: "100%", height: "150px" }}>
-      <Wave
-        fill="#6C63FF"
-        paused={false}
-        options={{
-          height: 25,
-          amplitude: 25,
-          speed: 0.25,
-          points: 5,
-        }}
-      />
-    </div>
-  );
+    <Wave
+      fill="#7cacf5"
+      paused={false}
+      style={{ display: 'flex' }}
+      options={{
+        height: 1,
+        amplitude: 30, 
+        speed: 0.15,
+        points: 4
+      }}
+    />
+  )
 }
