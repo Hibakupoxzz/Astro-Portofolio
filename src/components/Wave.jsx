e48b6ac432cba3dcb07892124b881743c9b785cd
Hibakupoxzz <duospirits@gmail.com>
2025-11-04 21:38:10 +0700

    Nia imutt woilahh

--- src/components/Wave.jsx
+++ src/components/Wave.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Wave from 'react-wavify'
+import React from "react";
+import Wave from "react-wavify";
 
-export default function WaveReact() {
+export default function WaveBackground() {
   return (
-    <Wave
-      fill="#7cacf5"
-      paused={false}
-      style={{ display: 'flex' }}
-      options={{
-        height: 1,
-        amplitude: 30, 
-        speed: 0.15,
-        points: 4
-      }}
-    />
-  )
+    <div className="relative w-full h-[300px]">
+      <Wave
+        fill="#7CB3FF"
+        paused={false}
+        options={{
+          height: 80,      // ini tinggi dasar gelombang (ubah jadi 100 kalau mau lebih tinggi)
+          amplitude: 40,   // tinggi naik-turunnya ombak
+          speed: 0.25,     // kecepatan animasi
+          points: 4        // jumlah gelombang
+        }}
+        className="absolute bottom-0 left-0 w-full h-[300px]"
+      />
+    </div>
+  );
 }
